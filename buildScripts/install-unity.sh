cf35bf883e351803441099ed4ef700e10838218e
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-17 05:50:53 -0400

    try other urls

--- buildScripts/install-unity.sh
+++ buildScripts/install-unity.sh
@@ -3,7 +3,7 @@
 # See https://unity3d.com/get-unity/download/archive
 # to get download URLs
 UNITY_DOWNLOAD_CACHE="$(pwd)/unity_download_cache"
-UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/fe82a0e88406/MacEditorInstaller/Unity.pkg"
+UNITY_OSX_PACKAGE_URL="https://download.unity3d.com/download_unity/fe82a0e88406/MacEditorInstaller/Unity-2019.2.6f1.pkg"
 UNITY_WINDOWS_TARGET_PACKAGE_URL="https://download.unity3d.com/download_unity/fe82a0e88406/MacEditorTargetInstaller/UnitySetup-Windows-Mono-Support-for-Editor-2019.2.6f1.pkg"
 
 
