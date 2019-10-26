33d083f3ea4c8970f3ae5540cd55d1c1ce0c2bee
Dominik Rutter <dominik.rutter@gmail.com>
2019-10-25 23:28:02 -0400

    Basic Scene Manager System

---
+++ Game/Assets/Global/Scripts/GameManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    static GameManager _instance; // Track instances of this class
+    void Start()
+    {
+        if (_instance != null) // If we exist, block creation
+        {
+            Destroy(this.gameObject);
+            Debug.LogError("Tried to instace the game manager twice!");
+            return;
+        }
+        else // If we don't exist, Create ourselves
+        {
+            _instance = this;
+            GameObject.DontDestroyOnLoad(this.gameObject); // Block deletion
+        }
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            SceneEngine.NextScene();
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            SceneEngine.PrevScene();
+        }
+    }
+
+}
---
+++ Game/Assets/Global/Scripts/SceneEngine.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public static class SceneEngine
+{
+    public static void NextScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex < SceneManager.sceneCountInBuildSettings - 1)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+    public static void PrevScene()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+        else
+        {
+            Application.Quit();
+        }
+    }
+    public static void LoadExactScene(int num)
+    {
+        if (num > 0 || num < SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(num);
+        }
+        else
+        {
+            Debug.LogError("Scene [" + num + "] does not exist");
+        }
+    }
+    public static void LoadExactScene(string str)
+    {
+        int num = SceneManager.GetSceneByName(str).buildIndex;
+        if (num > 0 || num < SceneManager.sceneCountInBuildSettings)
+        {
+            SceneManager.LoadScene(num);
+        }
+        else
+        {
+            Debug.LogError("Scene [" + num + "][" + str + "] does not exist");
+        }
+    }
+}
