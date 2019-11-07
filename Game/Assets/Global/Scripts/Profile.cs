d857269101fdbedd41209d730452532aaa47d3dd
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-07 04:02:31 -0500

    Create basic profile class

---
+++ Game/Assets/Global/Scripts/Profile.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Profile
+{
+    string name = "Player";
+    float playtime = 0.0f;
+    
+    public Profile(string name, float playtime)
+    {
+        if (name.Length > 10)
+        {
+            Debug.LogError("Name value [" + name + "] is too long (>10), setting default name");
+            this.name = "Player";
+        }
+        else
+        {
+            this.name = name;
+        }
+        if (playtime < 0.0)
+        {
+            Debug.LogError("Playtime is negative, only positive playtimes are possible");
+            this.playtime = 0.0f;
+        }
+        else
+        {
+            this.playtime = playtime;
+        }
+    }
+    public string getName()
+    {
+        return this.name;
+    }
+    public float getTime()
+    {
+        return this.playtime;
+    }
+}
