793d870e610983818ab7691f2ab5395c7d3586ab
Bruno Rico <54558283+AinzOolGown@users.noreply.github.com>
2019-11-12 00:45:17 -0500

    Import Bruno's Scripts for summer

---
+++ Game/Assets/Summer/Scripts/checkBoss2Death.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkBoss2Death : MonoBehaviour {
+
+
+    public static int boss2death = 0;
+
+
+    private int bossdeath;
+    // Use this for initialization
+    void Start () {
+        bossdeath = boss2death;
+    }
+
+    IEnumerator endLevel()
+    {
+
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene("LevelSelect");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (boss2death > bossdeath)
+        {
+            StartCoroutine("endLevel");
+        }
+    }
+}
