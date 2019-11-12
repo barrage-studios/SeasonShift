87e7e5d63de84dfc9704ec279c1953e4c43cf34e
Dominik Rutter <dominik.rutter@gmail.com>
2019-11-11 23:33:31 -0500

    Add Level select manager for handling buttons

---
+++ Game/Assets/Global/Scripts/LevelSelectManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class LevelSelectManager : MonoBehaviour
+{
+    public Button[] buttons;
+
+    void Start()
+    {
+        for (int i = 0; i <= GameData._instance.playerProfile.getLevelsCleared(); i++)
+        {
+            buttons[i].interactable = true;
+        }
+    }
+    public void SelectLevel(int level)
+    {
+        SceneEngine.PushScene(SceneEngine.Scenes.GAME);
+    }
+}
