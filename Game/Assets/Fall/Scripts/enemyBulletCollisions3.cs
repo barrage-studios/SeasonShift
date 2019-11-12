7ba46414df42f00cd1cf3829306fbf637b128d7f
Bruno Rico <54558283+AinzOolGown@users.noreply.github.com>
2019-11-12 00:45:54 -0500

    Import Bruno's Scripts for fall

---
+++ Game/Assets/Fall/Scripts/enemyBulletCollisions3.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyBulletCollisions3 : MonoBehaviour {
+
+    public int playerBulletLayer;
+
+    public int life;
+
+    public GameObject enemy;
+
+    public int pointsPerHit;
+
+
+
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+
+        Debug.Log("OnCollisionEnter2D");
+
+        if (col.gameObject.layer == playerBulletLayer)
+        {
+
+            life = life - 1;
+
+            Destroy(col.gameObject);
+
+            initializeScoreCounter.lev1ScoreCounter = initializeScoreCounter.lev1ScoreCounter + pointsPerHit;
+
+            if (life <= 0)
+            {
+
+                StartCoroutine("death");
+
+            }
+
+
+        }
+
+
+
+
+    }
+
+    IEnumerator death()
+    {
+        yield return new WaitForSeconds(.05f);
+
+        if (this.gameObject.layer == 12)
+        {
+            checkBoss3Death.boss3death = checkBoss3Death.boss3death + 1;
+        }
+
+
+        Destroy(enemy.gameObject);
+
+    }
+}
