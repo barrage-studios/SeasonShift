2c7d491e8fecc7a343a40de1a732c15e7478ac2c
Bruno Rico <54558283+AinzOolGown@users.noreply.github.com>
2019-11-12 00:46:42 -0500

    Import Bruno's Scripts for winter

---
+++ Game/Assets/Winter/Scripts/checkBoss4Death.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class checkBoss4Death : MonoBehaviour {
+
+
+    public static int boss4death = 0;
+
+
+
+    private int bossdeath;
+    // Use this for initialization
+    void Start () {
+        bossdeath = boss4death;
+    }
+
+    IEnumerator endLevel()
+    {
+
+        yield return new WaitForSeconds(5);
+
+        SceneManager.LoadScene("LevelSelect");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (boss4death > bossdeath)
+        {
+            StartCoroutine("endLevel");
+        }
+    }
+}
