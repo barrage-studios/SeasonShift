ff4d41dd77be61e9a9d52de4273ca3e3b3c45662
Bruno Rico <54558283+AinzOolGown@users.noreply.github.com>
2019-11-13 19:13:37 -0500

    Modified playerLife script
    added the tracking of the player spawn position and removed live bug that would remove collisions for the last life

--- Game/Assets/Global/Bruno Scripts/playerLives.cs
+++ Game/Assets/Global/Bruno Scripts/playerLives.cs
@@ -1,10 +1,11 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class playerLives : MonoBehaviour {
+public class playerLives : MonoBehaviour
+{
 
     public GameObject initialspawnPos;
     public GameObject playerPrefab;
@@ -14,10 +15,10 @@ public class playerLives : MonoBehaviour {
     public static int deathDetect = 0;
     public int startAm = 3;
 
-	// Use this for initialization
-	void Start () {
-        lives = startAm;
-        deathDetect = 0;
+    // Use this for initialization
+    void Start()
+    {
+        lives = lives - 1;
     }
 
     IEnumerator trueDeath()
@@ -27,25 +28,28 @@ public class playerLives : MonoBehaviour {
         SceneManager.LoadScene("Death");
 
     }
-	
-	// Update is called once per frame
-	void Update () {
-        if ((deathDetect > 0) & (lives > 0) & (playerInvincibility.isKillable)) {
-            Vector3 pos = new Vector3(-5.41f, -8.69f, 0f);
-            Quaternion angle = Quaternion.Euler(0f,0f,0f);
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((deathDetect > 0) & (lives > 0) & (playerManager.isKillable))
+        {
+            Vector3 pos = new Vector3(initialspawnPos.gameObject.transform.position.x, initialspawnPos.gameObject.transform.position.x, 0f);
+            Quaternion angle = Quaternion.Euler(0f, 0f, 0f);
 
             if (lives > 0)
             {
                 Instantiate(playerPrefab, pos, angle);
                 deathDetect -= 1;
                 lives = lives - 1;
-                
+
             }
-            
-            
+
+
         }
 
-        if(lives <= 0 && deathDetect == 1){
+        if (lives <= 0 && deathDetect == 1)
+        {
             StartCoroutine("trueDeath");
         }
 
